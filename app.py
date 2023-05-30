9f1a27dac6af17582f2d916593d4f0a60312dd09
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-05-30 16:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
 import streamlit as st
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver import FirefoxOptions
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 
-def scrape_data(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Run Chrome in headless mode
-
-    # Specify the path to the ChromeDriver binary
-    #chromedriver_path = '/home/appuser/.wdm/drivers/chromedriver/linux64/113.0.5672.63/chromedriver'
-
-    # Set up the webdriver with the executable_path parameter
-    #driver = webdriver.Chrome(executable_path=chromedriver_path)
-
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
-
-    driver.get(url)
-        # Perform web scraping actions using driver
-
-        # Example: Scrape and display the page title
-    title = driver.title
-    st.write(f"Page Title: {title}")
-
-def main():
-    st.title("Web Scraping with Selenium on Streamlit Cloud")
-
-    # Example scraping URL
-    url = st.text_input("Enter a URL to scrape:")
-    if st.button("Scrape"):
-        scrape_data(url)
-
-if __name__ == "__main__":
-    main()
+browser.get('http://example.com')
+st.write(browser.page_source)
