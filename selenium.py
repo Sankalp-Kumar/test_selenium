f6cc0642a4d13f9166411aaf0d4693189e9d2d7c
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-05-30 12:53:20 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,9 +1,5 @@
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-                
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
