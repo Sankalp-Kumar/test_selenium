635987081292af37febb4fb40db6e113473e68bb
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-05-30 14:54:00 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,16 +1,11 @@
 import streamlit as st
-from undetected_chromedriver import Chrome, ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 
 def scrape_data(url):
-    options = ChromeOptions()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Run Chrome in headless mode
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
 
-    with Chrome(options=options) as driver:
+    with webdriver.Chrome(options=options) as driver:
         driver.get(url)
         # Perform web scraping actions using driver
 
@@ -18,12 +13,8 @@ def scrape_data(url):
         title = driver.title
         st.write(f"Page Title: {title}")
 
-        # Example: Scrape and display a specific element
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1")))
-        st.write(f"Element Text: {element.text}")
-
 def main():
-    st.title("Web Scraping with Streamlit and Undetected Chrome Driver")
+    st.title("Web Scraping with Selenium on Streamlit Cloud")
 
     # Example scraping URL
     url = st.text_input("Enter a URL to scrape:")
