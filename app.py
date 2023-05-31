5495b8225b1ffab201f790b7159d8e92c803c0f6
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-05-31 13:20:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from undetected_chromedriver import Chrome, ChromeOptions
+#from undetected_chromedriver import Chrome, ChromeOptions
 
 # Set up ChromeOptions
-options = ChromeOptions()
+#options = ChromeOptions()
 
 # Set the path to the Chrome binary
 #options.binary_location = '/usr/bin/google-chrome-stable'  # Adjust this path if needed
@@ -10,14 +10,33 @@ options = ChromeOptions()
 # options.add_argument('--headless')  # Uncomment this line if you want to run in headless mode
 
 # Set up the Undetected ChromeDriver
-driver = Chrome(options=options)
+#driver = Chrome(options=options)
 
 # Perform your web scraping operations
-driver.get("https://www.amazon.in")
+#driver.get("https://www.amazon.in")
 
-title = driver.title
+#title = driver.title
 
-st.write(title)
+#st.write(title)
 
 # Close the browser
-driver.quit()
+#driver.quit()
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+
+driver = get_driver()
+driver.get("http://example.com")
+
+st.code(driver.page_source)
