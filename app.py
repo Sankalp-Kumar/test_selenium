7abe39b32c27dfe087fbab8f6aead8db7c92ad64
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-05-31 12:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import streamlit as st
-import os, sys
-
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from webdriver_manager.chrome import ChromeDriverManager
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+# Set up the Chrome webdriver using webdriver_manager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Open the webpage
+driver.get('https://www.amazon.com')
+
+# Scrape the title of the webpage
+title = driver.title
+st.write(title)
+
+# Close the browser
+driver.quit()
+
