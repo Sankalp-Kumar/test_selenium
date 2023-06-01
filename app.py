d3a21c07f13d3a5c07bf9ad885b83d7d43a6f908
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-06-01 13:41:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-driver.get("http://example.com")
+driver.get("http://www.amazon.com")
 
-st.code(driver.page_source)
+st.write(driver.title)
