5f3002221448e23be5192680e5a978809beeaaa3
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-06-16 14:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,20 +23,21 @@
 #driver.quit()
 
 import streamlit as st
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.cache_resource
+@st.experimental_singleton
 def get_driver():
-    #return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-     return webdriver.Chrome(service=Service(ChromeDriverManager.install()), options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-driver.get("http://www.amazon.com")
+driver.get('http://example.com')
 
-st.write(driver.title)
+st.code(driver.page_source)
