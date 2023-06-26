10835ecefcbfc6f6fb51dcbba31f0f2fa36cb39e
Sankalp Kumar <41043701+Sankalp-Kumar@users.noreply.github.com>
2023-06-26 12:52:16 +0530

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -300,7 +300,7 @@ if master_data :
 
                 #DataFrameDict[routes]
 
-                import undetected_chromedriver as uc
+                #import undetected_chromedriver as uc
                 from webdriver_manager.chrome import ChromeDriverManager
                 from selenium.webdriver.common.by import By
                 from selenium.webdriver.chrome.service import Service
